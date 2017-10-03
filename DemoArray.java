2a9697588fbe497b443249b0b43df3299ee0fc38
Sky <31314981+AlyssaXD@users.noreply.github.com>
2017-10-03 09:02:56 -0500

    Add files via upload

---
+++ DemoArray.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class DemoArray 
+{
+
+	public static void main(String[] args) 
+	{
+	      double[] salaries = {6.25, 6.55, 10.25, 16.85};
+	   
+	      System.out.println("Salaries one by one are:");
+	      
+	      for(int x = 0; x < salaries.length; ++x)
+	    	  System.out.println(salaries[x]);
+	}
+
+}
